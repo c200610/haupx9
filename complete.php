29496b8e56c76b0d333953c0012a749c89a63a1e
c200610 <c200610@ccg.ac.jp>
2021-11-05 01:37:55 +0000

    PDO work first commit 10/28

--- complete.php
+++ complete.php
@@ -1,11 +1,43 @@
-<?php
-  session_start();
+<?php 
 
-  $name = $_SESSION['name'];
-  $hobby = $_SESSION['email'];
-  $gender = $_SESSION['gender'];
-?>
+  session_start(); 
 
+  require_once("functions.php"); 
+  $name = $_SESSION['name']; 
+
+  $email = $_SESSION['email']; 
+
+  $gender = $_SESSION['gender']; 
+
+
+$dbh = db_conn();      // データベース接続 
+try{ 
+
+      $sql = "INSERT INTO user (email, name, gender) VALUE (:email, :name, :gender)"; 
+
+      $stmt = $dbh->prepare($sql);                           //クエリの実行準備 
+
+      $stmt->bindValue(':email', $email, PDO::PARAM_STR);      //バインド:プレースホルダ―を埋める 
+
+      $stmt->bindValue(':name', $name, PDO::PARAM_STR);  //バインド:プレースホルダ―を埋める 
+
+      $stmt->bindValue(':gender', $gender, PDO::PARAM_INT); //バインド:プレースホルダーを埋める 
+
+      $stmt->execute();                                        //クエリの実行 
+
+      $dbh = null;                                             //MySQL接続解除 
+
+}catch (PDOException $e){ 
+
+      echo($e->getMessage()); 
+
+    die(); 
+
+} 
+
+?> 
+
+  
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -24,7 +56,7 @@
 </div>
 <hr>
 <p>名前は <?php echo $name;?> さん</p>
-<p>メールアドレスは <?php echo $hobby;?> </p>
+<p>メールアドレスは <?php echo $email;?> </p>
 
 <p>性別は <?php if( $gender === "1" ){ echo '男性'; }
 		elseif( $gender === "2" ){ echo '女性'; }
